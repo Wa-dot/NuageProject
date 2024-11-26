e0719cb3b56ff635e136991be98fc3c12aa15c65
wa-dot <wandrilleguiraud@hotmail.com>
2024-11-26 14:48:32 +0100

    Rename ressource group, terra plan start

--- variable.tf
+++ variable.tf
@@ -1,15 +1,17 @@
 variable "resource_group_name" {
   type        = string
-  description = "Projet Nuage"
-}
-
-variable "location" {
-  type        = string
-  description = "Localisation Azure"
-  default     = "West Europe"
+  description = "Nom du groupe de ressources"
+  default     = "rg_nuage_project"  # Convention de nommage: rg-[nom-projet]
 }
 
 variable "storage_account_name" {
   type        = string
   description = "Nom du compte de stockage"
+  default     = "stnuageproject"    # Doit être unique et en minuscules, sans caractères spéciaux
+}
+
+variable "location" {
+  type        = string
+  description = "Localisation Azure"
+  default     = "francecentral" 
 }
