153023452230d19d92bfd9d63f339e91f39ee954
wa-dot <wandrilleguiraud@hotmail.com>
2024-11-27 12:25:06 +0100

    Delete np_ in var name

--- infrastructure/modules/postgreSQL/main.tf
+++ infrastructure/modules/postgreSQL/main.tf
@@ -1,27 +1,27 @@
-resource "azurerm_private_dns_zone" "np_dns" {
+resource "azurerm_private_dns_zone" "dns" {
   name                = "np-domain.postgres.database.azure.com"
   resource_group_name = azurerm_resource_group.example.name # à modifier
 }
 
-resource "azurerm_postgresql_flexible_server" "np_postgresql" {
-  name                          = var.np_postgresql_server_name
-  location                      = var.np_physical_loc
-  version                       = var.np_db_version
-  resource_group_name           = var.np_rg_name
-  delegated_subnet_id           = var.np_delegated_subnet_id
-  private_dns_zone_id           = azurerm_private_dns_zone.np_dns.id
-  administrator_login           = var.np_admin_username
-  administrator_password        = var.np_admin_password
-  storage_mb   = var.np_storage_mb
-  sku_name   = var.np_sku_name
+resource "azurerm_postgresql_flexible_server" "postgresql" {
+  name                          = var.postgresql_server_name
+  location                      = var.physical_loc
+  version                       = var.db_version
+  resource_group_name           = var.rg_name
+  delegated_subnet_id           = var.delegated_subnet_id
+  private_dns_zone_id           = azurerm_private_dns_zone.dns.id
+  administrator_login           = var.admin_username
+  administrator_password        = var.admin_password
+  storage_mb   = var.storage_mb
+  sku_name   = var.sku_name
 
   public_network_access_enabled = false
 
 }
 
-resource "azurerm_postgresql_flexible_server_database" "np_database" {
-  name      = var.np_db_name
-  server_id = azurerm_postgresql_flexible_server.np_postgresql.id
+resource "azurerm_postgresql_flexible_server_database" "database" {
+  name      = var.db_name
+  server_id = azurerm_postgresql_flexible_server.postgresql.id
   collation = "en_US.utf8"
   charset   = "utf8"
 }
--- infrastructure/modules/postgreSQL/variables.tf
+++ infrastructure/modules/postgreSQL/variables.tf
@@ -1,50 +1,50 @@
-variable "np_postgresql_server_name" {
+variable "postgresql_server_name" {
     description = "Name given to the postgresql server"
     type = string
 }
 
-variable "np_rg_name" {
+variable "rg_name" {
     description = "Name of the resource group in Azure"
     type = string
 }
 
-variable "np_physical_loc" {
+variable "physical_loc" {
     description = "Location of the Azure resources"
     type = string
 }
 
-variable "np_delegated_subnet_id" {
+variable "delegated_subnet_id" {
     description = "ID of the delegated sub network for the PostgreSQL Flexible Server"
     type = string
 }
 
-variable "np_admin_username" {
+variable "admin_username" {
     description = "Username of the administrator of the PostgreSQL"
     type = string
 }
 
-variable "np_admin_password" {
+variable "admin_password" {
     description = "Secured password of the PostgreSQL administrator"
     type = string
     sensitive = true
 }
 
-variable "np_db_name" {
+variable "db_name" {
     description = "Name given to the created database"
     type = string
 }
 
-variable "np_sku_name" {
+variable "sku_name" {
     description = "SKU name"
     type = string
 }
 
-variable "np_storage_mb" {
+variable "storage_mb" {
     description = "Storage capacity for PostgreSQL in Mo"
     type = number
 }
 
-variable "np_db_version" {
+variable "db_version" {
     description = "Version of the PostgreSQL server"
     type = string
 }
