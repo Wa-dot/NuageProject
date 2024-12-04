248b51ff09a3e2ae34a7ed78b062aee6a096e5f8
wa-dot <wandrilleguiraud@hotmail.com>
2024-12-04 17:59:57 +0100

    Check with terra apply

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -13,3 +13,15 @@ variable "storage_account_name" {
   type        = string
   description = "Nom du compte de stockage"
 }
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+  sensitive   = true
+}
