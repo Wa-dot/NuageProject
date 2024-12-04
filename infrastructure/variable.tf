0296775b5e17b4ea57fb0d95c99ce190bb2dd433
wa-dot <wandrilleguiraud@hotmail.com>
2024-12-04 18:18:28 +0100

    Add tenant ans sub ids.

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -15,3 +15,15 @@ variable "location" {
   description = "Localisation Azure"
   default     = "francecentral" 
 }
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+  sensitive   = true
+}
