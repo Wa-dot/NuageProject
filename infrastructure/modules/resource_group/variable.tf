45b6a7db1cb7b8386e66eff19aaf84beddd15663
HanaDelport <hana.delport@student.junia.com>
2024-12-06 13:14:15 +0100

    feat(infrastructure) : Added code in variables and outputs.

---
+++ infrastructure/modules/resource_group/variable.tf
@@ -0,0 +1,9 @@
+variable "ressource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Resource group and modules physical location"
+  type        = string
+}
